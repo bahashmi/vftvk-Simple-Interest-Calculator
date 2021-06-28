daadcc9eb2bde414e50817c611f1864d0680896c
bhashmi88 <bhashmi88@gmail.com>
2021-06-27 23:33:37 -0700

    additional changes

--- script.js
+++ script.js
@@ -12,11 +12,18 @@ if ((parseInt(p) === 0) || (parseInt(p) < 0) || (p === "")) {
 else {
     result  = document.getElementById("result").innerHTML = parseInt(  p*y*(r/100))
     result1  = document.getElementById("result1").innerHTML =  parseFloat(r) +"%"
-    principalvalue = document.getElementById("principalvalue").innerHTML =  "If you deposit " + p +","
+    principalvalue = document.getElementById("principalvalue").innerHTML =  "If you deposit " +  p +","
     ratevalue = document.getElementById("ratevalue").innerHTML =  "at an interest rate of  " +  r + "%."
     text = document.getElementById("text").innerHTML =  "You will receive an amount of " +  result +','
     text2 = document.getElementById("text2").innerHTML =  "in the year " +  (2020 + parseInt(y))
 }
 
+          
+
 }
+
+function myFunction() {
+    var x = document.getElementById("rate").value;
+    document.getElementById("result1").innerHTML = "" + x +"%";
+  }
         
